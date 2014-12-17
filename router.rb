0d0378d98bfcda8fbb0c9b60a1080994a858ffe7
rgpass <rgpass@gmail.com>
2014-12-17 18:23:37 -0500

    Add structure for blog

--- router.rb
+++ router.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
 get '/' do
-  "Hello world"
+  erb :portfolio
+end
+
+get '/posts' do
+  erb :posts
+end
+
+get '/creating-blog-sinatra-syntax-highlighting' do
+  erb 'posts/n_001'.to_sym
 end
